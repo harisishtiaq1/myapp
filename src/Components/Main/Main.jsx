115e41b60b514c0c3dac4a25079590543ba4b0fb
harisishtiaq1 <harisharry232@gmail.com>
2023-02-07 11:03:35 +0500

    CHanges

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Main() {
   return (
-<section className="text-center ">
-  <div className="card mx-4 mx-md-5 shadow-5-strong">
+<section className="text-center">
+  <div className="p-5 bg-image" style={{backgroundColor:"white"}}></div>
+  <div className="card mx-4 mx-md-5 shadow-5-strong" >
     <div className="card-body py-5 px-md-5">
-
       <div className="row d-flex justify-content-center">
         <div className="col-lg-8">
           <h2 className="fw-bold mb-5">Sign up now</h2>
@@ -24,18 +24,22 @@ function Main() {
                 </div>
               </div>
             </div>
-
-            
             <div className="form-outline mb-4">
               <input type="email" id="form3Example3" className="form-control" />
               <label className="form-label" for="form3Example3">Email address</label>
             </div>
-
             <div className="form-outline mb-4">
               <input type="password" id="form3Example4" className="form-control" />
               <label className="form-label" for="form3Example4">Password</label>
             </div>
+            <div className="form-check d-flex justify-content-center mb-4">
+              <input className="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+              <label className="form-check-label" for="form2Example33">
+                Subscribe to our newsletter
+              </label>
+            </div>
 
+            
             <button type="submit" className="btn btn-primary btn-block mb-4">
               Sign up
             </button>
