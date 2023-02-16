de8dbf6fcdf4f1207db6d5dd04b8a9152ebd2215
harisishtiaq1 <harisharry232@gmail.com>
2023-02-16 12:56:29 +0500

    Changes

--- src/Components/Task/AlbumLayout.jsx
+++ src/Components/Task/AlbumLayout.jsx
@@ -1,20 +1,87 @@
-import { AppBar, Avatar, createTheme, CssBaseline, ThemeProvider, Toolbar, Typography } from '@mui/material'
-import React from 'react'
-import image from "./Image.jpg"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from '@mui/material';
+import logo from "./download.png"
 
-const nabar=['Home','Layouts','Apps','Customization','Features','Document']
-function AlbumLayout() {
-    const theme=createTheme()
+const navItems = ['Home', 'Layputs', 'Apps','Customization','Features','Document'];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center',display:'flex',justifyContent:'space-between' }}>
+      <Link href='/'>
+        <Box component='img' alt='logo' src={logo} ></Box>
+      </Link>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item}>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <AppBar position="relative">
+    <Box sx={{ display:'flex',justifyContent:'space-between' }} >
+      <CssBaseline />
+      <AppBar component="nav">
         <Toolbar>
-        <Avatar alt="Haris" src={image} />
+          <Link href='/'>
+            <Box component="img" alt='image/logo' src={logo} sx={{height:35,marginRight:3}}></Box>
+          </Link>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
-    </ThemeProvider>
-  )
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box',},
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
 }
 
-export default AlbumLayout
+
+export default DrawerAppBar;
