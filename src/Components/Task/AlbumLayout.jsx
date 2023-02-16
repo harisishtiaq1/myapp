d9d2a08ca908a61626de1291718623dbf79d7369
harisishtiaq1 <harisharry232@gmail.com>
2023-02-16 15:51:23 +0500

    Changes

--- src/Components/Task/AlbumLayout.jsx
+++ src/Components/Task/AlbumLayout.jsx
@@ -3,7 +3,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -119,7 +118,6 @@ function AlbumLayout() {
             </Container>
             </Box>
     </main>
-    
     </ThemeProvider>
   );
 }
